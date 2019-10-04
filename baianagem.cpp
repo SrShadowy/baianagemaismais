24c48baf22f0ab549f86d33c9ae4437b14d96c87
SrShadowy <43967231+SrShadowy@users.noreply.github.com>
2019-10-04 19:23:50 -0300

    primeira

---
+++ baianagem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "linguabaiana.h"
+
+macumba oxente() 
+exu
+para naue capueira acaraje
+
+desgraca rapaz  "\noxente\n" acaraje
+macumba x acaraje
+oferenda galinha x acaraje
+x = x mais tamainDe(meia)acaraje
+desgraca rapaz x rapaz finDePapo acaraje
+enxu
+
---
+++ linguabaiana.h
@@ -0,0 +1,30 @@
+//Tipos
+#define macumba int
+#define vazi	void
+#define verdadi bool
+#define meia	float
+#define pokin	byte
+
+// definicoes
+#define oxente 	main
+#define para 	using
+#define naue	namespace
+#define capueira std
+#define desgraca cout
+#define oferenda cin
+#define tamainDe sizeof
+#define finDePapo endl
+
+//Simbolos
+#define rapaz	<<
+#define exu	{
+#define enxu	}
+#define acaraje	;
+#define galinha	>>
+#define mais	+
+#define menos	-
+#define divide	/
+#define vezes	*
+
+
+
